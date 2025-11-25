411e0d1e1ab0e2abcc2331b51143c9b3d6f2aeb4
jongbot1 <kpp6839@gmail.com>
2025-11-25 21:31:40 +0900

    jong 11/25/embedding

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+
 from app.routers.chat import router as chat_router
+from app.routers.update import router as update_router
+
 
 app = FastAPI(title="GigWork Chatbot API")
 
+
 app.include_router(chat_router, prefix="/chat", tags=["Chatbot"])
+app.include_router(update_router, prefix="/update", tags=["Update"])
