c4d03cf49244ce616abaa7be27d3fca6922ff951
jongbot1 <kpp6839@gmail.com>
2025-11-27 17:46:25 +0900

    routerinput

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 
 from app.routers.chat import router as chat_router
+from app.routers.update import router as update_router
 from app.routers.update_conditions import router as condition_update_router
 
 app = FastAPI(title="GigWork Chatbot API")
 
-app.include_router(chat_router, prefix="/chat", tags=["Chatbot"])
+app.include_router(update_router, prefix="/update", tags=["Update"])
+app.include_router(condition_update_router, prefix="/chat", tags=["Chatbot"])
 
 app.include_router(condition_update_router, prefix="/conditions", tags=["Conditions"])
